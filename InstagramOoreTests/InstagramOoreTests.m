17d6f5620fd62c03fc07f8c69791a33eaca4d4d2
OoreFasawe <oore95@gmail.com>
2022-06-26 22:50:05 -0700

    Initial Commit

---
+++ InstagramOoreTests/InstagramOoreTests.m
@@ -0,0 +1,36 @@
+//
+//  InstagramOoreTests.m
+//  InstagramOoreTests
+//
+//  Created by Oore Fasawe on 6/26/22.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface InstagramOoreTests : XCTestCase
+
+@end
+
+@implementation InstagramOoreTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
