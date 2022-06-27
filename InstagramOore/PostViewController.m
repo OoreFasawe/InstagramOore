8756a92fa61546ed124a7c36d9c667d1e48e7a8b
OoreFasawe <oore95@gmail.com>
2022-06-27 14:23:38 -0700

    Logout functionality implemented

---
+++ InstagramOore/PostViewController.m
@@ -0,0 +1,54 @@
+//
+//  PostViewController.m
+//  InstagramOore
+//
+//  Created by Oore Fasawe on 6/27/22.
+//
+
+#import "PostViewController.h"
+#import "SceneDelegate.h"
+#import "LoginViewController.h"
+#import <Parse/Parse.h>
+
+@interface PostViewController ()
+
+@end
+
+@implementation PostViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+- (IBAction)didTapLogout:(id)sender {
+    
+    [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {
+        // PFUser.current() will now be nil
+        
+        if (error)
+        {}
+        else{
+            SceneDelegate *sceneDelegate = (SceneDelegate *)self.view.window.windowScene.delegate;
+
+            UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+            LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+            sceneDelegate.window.rootViewController = loginViewController;
+            NSLog(@"Reach here");
+        }
+    }];
+    
+    
+
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
